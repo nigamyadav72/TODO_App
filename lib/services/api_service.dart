0824ffc221d67035718b3b9e4634dcaf6aae9a4b
NIGAM YADAV <71872897+nigamyadav72@users.noreply.github.com>
2026-03-05 08:56:42 +0545

    final push

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -6,13 +6,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../models/task.dart';
 
 class ApiService {
-  static String get baseUrl {
-    if (Platform.isAndroid) {
-      return 'http://10.0.2.2:8000/api';
-    } else {
-      return 'http://127.0.0.1:8000/api';
-    }
-  }
+  static String get baseUrl => 'https://todo-app-x5nq.onrender.com/api';
 
   static Future<String?> getToken() async {
     final prefs = await SharedPreferences.getInstance();
